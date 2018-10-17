4a44bbe06d5da22980f86b0a51b18c24a3244e7c
Jared Lander <info@jaredlander.com>
2018-10-17 13:55:20 -0400

    Built out inputs and output for plots panel

--- app/ui.r
+++ app/ui.r
@@ -1,7 +1,33 @@
 library(shiny)
 
+plotsPanel <- tabPanel(
+    title='Plots',
+    fluidRow(
+        column(
+            width=4,
+            selectInput(
+                inputId='ColumnName',
+                label='Choose a column',
+                choices=names(mtcars)
+            ),
+            sliderInput(
+                inputId='NumBins',
+                label='Choose the number of bins',
+                min=5, max=50, value=25
+            )
+        ),
+        column(
+            width=8,
+            plotOutput(
+                outputId='CarHistogram'
+            )
+        )
+    )
+)
+
 navbarPage(
     title='My First Shiny App',
+    selected='Plots',
     tabPanel(
         title='First Page',
         'Hello, there'
@@ -9,5 +35,6 @@ navbarPage(
     tabPanel(
         title='Second Page',
         'You must be going'
-    )
+    ),
+    plotsPanel
 )
